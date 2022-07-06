fa62922ba0cb3af08d8e686b72c20c2a3bf8f9b8
toMilanesi <tomi.milanesi@gmail.com>
2022-07-06 16:56:01 -0300

    Added header component with Links

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <h1>Magic Magicians</h1>
+      <nav>
+        <ul>
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/calculator'}>Calculator</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
