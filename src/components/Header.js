2ed579ba0367ac7729b94710db0e8a69cf21394c
toMilanesi <tomi.milanesi@gmail.com>
2022-07-06 18:57:32 -0300

    Added Home.js component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,13 @@ function Header() {
       <h1>Magic Magicians</h1>
       <nav>
         <ul>
-          <li><Link to={'/'}>Home</Link></li>
-          <li><Link to={'/calculator'}>Calculator</Link></li>
-          <li><Link to={'/about'}>About</Link></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/calculator">Calculator</Link></li>
+          <li><Link to="about">About</Link></li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from './Header';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to our page!</h1>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem labore
+            laboriosam, corporis ipsam, tenetur accusantium amet nemo architecto
+            quas sed sint est cumque dicta itaque voluptatibus numquam mollitia
+            vel assumenda.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            accusantium natus obcaecati rerum est error, incidunt dignissimos,
+            quia ut perspiciatis animi eius delectus neque alias soluta quisquam
+            aut culpa ratione.
+          </p>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export default Home;
