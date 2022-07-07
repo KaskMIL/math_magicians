cb38a0022bf4a8fbce05e2e7bea7be9818b1a31d
toMilanesi <tomi.milanesi@gmail.com>
2022-07-07 18:07:15 -0300

    Added tests to =

--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -18,3 +18,31 @@ describe('+/- button to return the inverse of a number', ()=>{
     expect(result).toEqual({total: '10', next: null, operation: null})
   })
 })
+
+describe('If button = . should be include on next', () => {
+  test('If next already has . shoould return the same object', () => {
+    const init = {total: null, next: '2.', operation: null};
+    const result = calculate(init, '.');
+    expect(result).toEqual(init);
+  })
+  test('If next doesnt has . should return next with .', () => {
+    const init = {total: null, next: '2', operation: null};
+    const result = calculate(init, '.');
+    expect(result).toEqual({total: null, next: '2.', operation: null})
+  })
+  test('If there is an operation already', () => {
+    const init = {total: null, next: null, operation: '+'};
+    const result = calculate(init, '.');
+    expect(result).toEqual({total: null, next: '0.', operation: '+'})
+  })
+  test('If there is an total already', () => {
+    const init = {total: '2.1', next: null, operation: null};
+    const result = calculate(init, '.');
+    expect(result).toEqual({})
+  })
+  test('If there is an operation already', () => {
+    const init = {total: null, next: null, operation: null};
+    const result = calculate(init, '.');
+    expect(result).toEqual({total: null, next: '0.', operation: null})
+  })
+})
