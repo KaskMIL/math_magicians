3e4182f79f812cb7223ed92643228c530f839b1c
toMilanesi <tomi.milanesi@gmail.com>
2022-07-06 21:29:22 -0300

    Added About styles module

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import styles from './styles/About.module.css';
 
 function About() {
   return (
     <>
       <Header />
-      <article>
+      <article className={styles.about}>
         <p>
           Mathematics is not about numbers, equation, computations or alogithms:
           It is about understanding.-William Paul Thurston
