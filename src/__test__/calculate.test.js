645c3a8df897aca1f0da39d7edd7b7c19f417639
dqarias <dino.quispea@gmail.com>
2022-07-07 15:47:42 -0500

    Add test for +/- button on calculate test

--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -5,3 +5,16 @@ test('AC button should return an null object', () => {
   const result = calculate(intial, 'AC');
   expect(result).toEqual({total: null, next: null, operation: null});
 })
+
+describe('+/- button to return the inverse of a number', ()=>{
+  test('next equal to 5 should return -5', ()=>{
+    const initial = {total: null, next: '5', operation: null}
+    const result = calculate(initial, '+/-');
+    expect(result).toEqual({total: null, next: '-5', operation: null})
+  })
+  test('total equal -10 should return 10', ()=>{
+    const initial = {total: '-10', next: null , operation: null}
+    const result = calculate(initial,'+/-');
+    expect(result).toEqual({total: '10', next: null, operation: null})
+  })
+})
